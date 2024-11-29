a9aab72df5e706561835ac837151378f2fc71667
boreq <boreq@0x46.net>
2024-11-29 13:02:24 +0100

    Remove snippet related stuff, remap rust-tools to ctrl-enter

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 rt.setup({
   server = {
     on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<C-Enter>", rt.hover_actions.hover_actions, { buffer = bufnr })
       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
@@ -23,11 +23,6 @@ rt.setup({
 local cmp = require'cmp'
 
 cmp.setup({
-    snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      end,
-    },
     window = {
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
@@ -42,7 +37,6 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' },
     }, {
       { name = 'buffer' },
     })
--- old.vim
+++ old.vim
@@ -12,8 +12,6 @@ Plug 'neovim/nvim-lspconfig'
 Plug 'simrat39/rust-tools.nvim'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-vsnip'
-Plug 'hrsh7th/vim-vsnip'
 call plug#end()
 
 " Misc
