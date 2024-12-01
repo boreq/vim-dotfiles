26a706bd8accbebe5b34d60c3f8ef82c4193e21f
boreq <boreq@0x46.net>
2024-12-01 03:48:33 +0100

    Keymap

--- init.lua
+++ init.lua
@@ -18,42 +18,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
 --  },
 --})
 
-vim.g.rustaceanvim = {
-  -- Plugin configuration
-  tools = {
-  },
-  -- LSP configuration
-  server = {
-    on_attach = function(client, bufnr)
-      -- you can also put keymaps in here
-      vim.keymap.set(
-          "n", 
-          "<leader>a", 
-          function()
-            vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-            -- or vim.lsp.buf.codeAction() if you don't want grouping.
-          end,
-          { silent = true, buffer = bufnr }
-        )
-        vim.keymap.set(
-          "n", 
-          "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-          function()
-            vim.cmd.RustLsp({'hover', 'actions'})
-          end,
-          { silent = true, buffer = bufnr }
-        )
-        end,
-    default_settings = {
-      -- rust-analyzer language server configuration
-      ['rust-analyzer'] = {
-      },
-    },
-  },
-  -- DAP configuration
-  dap = {
-  },
-}
+vim.keymap.set(
+  "n", 
+  "<leader>a", 
+  function()
+    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+    -- or vim.lsp.buf.codeAction() if you don't want grouping.
+  end,
+  { silent = true, buffer = bufnr }
+)
+vim.keymap.set(
+  "n", 
+  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+  function()
+    vim.cmd.RustLsp({'hover', 'actions'})
+  end,
+  { silent = true, buffer = bufnr }
+)
 
 local cmp = require'cmp'
 
